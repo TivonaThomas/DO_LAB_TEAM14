b7f24db0d7dfd42ed644cfef9049491c16c32d17
TivonaThomas <tivonakristinathomas.27csb@licet.ac.in>
2026-02-05 11:27:39 +0530

    firstcommit

---
+++ src/main/java/com/example/dolab/DolabApplication.java
@@ -0,0 +1,13 @@
+package com.example.dolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolabApplication.class, args);
+	}
+
+}
